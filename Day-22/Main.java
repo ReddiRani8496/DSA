03438af4515657b4f65abf76eddaa24b4c7a7247
reddirani8496 <reddiranibattuluri@gmail.com>
2023-08-26 09:32:59 +0530

    Day-22 programs done

---
+++ Day-22/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+      LinkedList list = new LinkedList();
+//      list.addFirst(10);
+//      list.addLast(20);
+//        list.addLast(30);
+//        list.addLast(40);
+//        list.addLast(50);
+//        list.addLast(60);
+//        list.addLast(70);
+//        list.addLast(80);
+//        list.addLast(90);
+      //  list.addLast(100);
+       var x = LinkedList.createLoop();
+        System.out.println(x.detectLoop());
+
+
+
+
+//        System.out.println(list.indexOf(10));
+//        System.out.println(list.valueOf(1));
+//        System.out.println(list.contains(10));
+//        list.printMiddle();
+//       list.printMiddle();
+
+    }
+}
+
