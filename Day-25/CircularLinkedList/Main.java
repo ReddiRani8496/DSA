f9c027707e94239ef8be9a81239f2244a660932d
reddirani8496 <reddiranibattuluri@gmail.com>
2023-08-29 18:15:43 +0530

    Day-25 programs done

--- Day-25/CircularLinkedList/Main.java
+++ Day-25/CircularLinkedList/Main.java
@@ -1,17 +1,41 @@
 public class Main {
     public static void main(String[] args) {
         CircularLinkedList list = new CircularLinkedList();
-//        list.addFirst(10);
-//        list.addLast(20);
-//        list.addLast(30);
-//        list.addLast(20);
-//        list.addLast(30);
+        list.addFirst(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+
+        System.out.println(list.contains(70));
+//        list.print();
+//        list.removeAt(0);
+//        System.out.println(list.size());
+//        list.print();
+//        list.removeAt(3);
+//        System.out.println(list.size());
+//        list.print();
+//        list.removeAt(1);
+//        list.removeAt(0);
+//        System.out.println(list.size());
+//        list.print();
+//        list.addAt(0,60);
+//        list.print();
+//        list.addAt(6,70);
+//        list.print();
+//        list.addAt(1,80);
+//        list.print();
+//        System.out.println(list.size());
+//        list.addAt(7,90);
+//        list.print();
+//        list.addAt(9,100);
+//        list.print();
 //
 //        list.print();
 //        list.addFirst(20);
 //        list.addLast(30);
 //        list.addLast(40);
-        System.out.println(list.lastIndexOf(10));
+//        System.out.println(list.lastIndexOf(10));
 
     }
 }
