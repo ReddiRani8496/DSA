3b007cc789f860a7791a9b506e19d5dfed6ad20d
reddirani8496 <reddiranibattuluri@gmail.com>
2023-09-16 17:52:46 +0530

    Day-37 programs done

---
+++ Day-37/Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+    public static void main(String[] args) {
+      LinkedList list = new LinkedList();
+      list.addLast(1);
+      list.addLast(20);
+      list.addLast(3);
+      list.addLast(40);
+      list.traversal(list.segregate(list.head));
+
+    }
+}
+
+
