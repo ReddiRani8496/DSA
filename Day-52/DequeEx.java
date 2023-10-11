a86e81029ed1c81ea296fb4a671011c0453fb733
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-11 21:10:02 +0530

    Day-52 programs done

--- Day-52/DequeEx.java
+++ Day-52/DequeEx.java
@@ -3,7 +3,9 @@ import java.util.Deque;
 
 public class DequeEx {
     public static void main(String[] args) {
+
         Deque<Integer> deque = new ArrayDeque<>();
+        
         deque.add(10);
         deque.addLast(20);
         deque.add(30);
