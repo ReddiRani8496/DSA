9126d51832c81e8985551006865cdef8ff589390
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-14 10:14:52 +0530

    Day-55 programs done

--- Day-55/SlidingWindowMax.java
+++ Day-55/SlidingWindowMax.java
@@ -6,23 +6,41 @@ public class SlidingWindowMax {
         int arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6};
         int k = 4;
         maxSub(arr,k);
+        maxSub1(arr,k);
     }
     public static void maxSub(int arr[],int k) {
         Deque<Integer> deque = new ArrayDeque<>();
+
         for (int i = 0; i < k; i++) {
             while (deque.size()>0 && arr[i] >= arr[deque.peekLast()])
                 deque.pollLast();
             deque.offerLast(i);
         }
+
         for (int i = k; i < arr.length; i++) {
-            System.out.println(arr[deque.peekFirst()]);
+
+            System.out.print(arr[deque.peekFirst()]+" ");
+
             while (deque.size()>0 && deque.peekFirst() <= i-k)
                 deque.pollFirst();
+
             while (deque.size()>0 && arr[i] >= arr[deque.peekLast()])
                 deque.pollLast();
             deque.offerLast(i);
         }
         System.out.println(arr[deque.peekFirst()]);
     }
-}
 
+    
+    public static void maxSub1(int arr[], int k) {
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i <= arr.length-k; i++) {
+            for (int j = i; j < i+k; j++) {
+                if(arr[j]>max)
+                    max = arr[j];
+            }
+            System.out.print(max+" ");
+        }
+        
+    }
+}
