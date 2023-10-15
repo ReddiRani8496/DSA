a94a239a42a8dcd0e8ec9734290f2dffcfc20e1d
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-15 22:18:48 +0530

    Day-57 programs done

---
+++ Day-57/FirstRepeatingChar.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstRepeatingChar {
+    public static void main(String[] args) {
+        
+        String str = "abcdab";
+        char ch = firstRepeatingChar(str);
+        System.out.println(ch);
+    }
+
+    private static char firstRepeatingChar(String str) {
+        
+        Set<Character> set = new HashSet<>();
+        var chars = str.toCharArray();
+        
+        for(var x : chars) {
+            if(set.contains(x))
+                return x;
+            set.add(x);
+        }
+        
+        return Character.MIN_VALUE;
+    }
+}
