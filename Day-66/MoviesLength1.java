fcf43f372501d6e97cb7a99b2f62f66a66fbfdaa
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-28 11:40:51 +0530

    Day-66 programs done

--- Day-66/MoviesLength1.java
+++ Day-66/MoviesLength1.java
@@ -3,8 +3,8 @@ import java.util.HashSet;
 public class MoviesLength1 {
     public static void main(String[] args) {
         int movieLength = 120;
-        int[] movies = {70,40,60,50,30};
-        System.out.println(isWatchTwoMovies(movies,movieLength));
+        int[] movies = {70,40,60,10,30};
+        System.out.println(isWatchTwoMovies1(movies,movieLength));
     }
     public static boolean isWatchTwoMovies(int[] movies,int movieLength) {
         HashSet<Integer> set = new HashSet<>();
@@ -15,6 +15,17 @@ public class MoviesLength1 {
            if(set.contains(required) && required!=y)
                return true;
        }
+        return false;
+    }
+    public static boolean isWatchTwoMovies1(int[] movies,int movieLength) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(var y : movies) {
+            int required = movieLength-y;
+            if(set.contains(required))
+                return true;
+            set.add(y);
+        }
         return false;
     }
 }
