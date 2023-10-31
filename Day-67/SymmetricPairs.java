5c77a4d7dfa7a9139b4b898253df89503d0d424c
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-31 11:12:08 +0530

    Day-67 programs doen

---
+++ Day-67/SymmetricPairs.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SymmetricPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols =sc.nextInt();
+        int arr[][] = new int[rows][cols];
+        for(int i=0;i<rows;i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+
+        }
+        symmetricPairs(arr);
+    }
+    public static void symmetricPairs(int[][] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int[] x : arr) {
+            int first = x[0];
+            int second = x[1];
+            Integer required = map.get(second);
+            if (required != null && required == first)
+                System.out.println(first + " " + second);
+            else
+                map.put(first, second);
+        }
+    }
+}
