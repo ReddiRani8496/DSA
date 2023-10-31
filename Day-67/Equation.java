08f972e55777478d27698ea4b971024465f9aebd
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-31 22:47:00 +0530

    Day-67 programs done

--- Day-67/Equation.java
+++ Day-67/Equation.java
@@ -1,6 +1,13 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Equation {
     public static void main(String[] args) {
         int arr[] ={1,2,3,4,5,6,7,8};
+        pairsNaiveApproach(arr);
+        pairsUsingHashing(arr);
+    }
+    public static void pairsNaiveApproach(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
             int sum = arr[i];
             for(int k=i+1;k<arr.length;k++) {
@@ -12,5 +19,20 @@ public class Equation {
             }
         }
     }
-}
 
+    public static void pairsUsingHashing(int[] arr) {
+        HashMap<Integer,int[]> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                int sum = arr[i]+arr[j];
+                if(map.containsKey(sum)) {
+                    int elements[] = map.get(sum);
+                    System.out.print("(" + arr[i] +","+arr[j]+")\t");
+                    System.out.println("(" + elements[0] +","+elements[1]+")");
+                } else {
+                    map.put(sum,new int[] {arr[i],arr[j]});
+                }
+            }
+        }
+    }
+}
