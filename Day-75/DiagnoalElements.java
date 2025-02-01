638f66a7f61cccec8fee1662748f3346f622eb78
reddirani8496 <reddiranibattuluri@gmail.com>
2025-02-01 23:13:37 +0530

    Printing diagonal elements program developed

--- Day-75/DiagnoalElements.java
+++ Day-75/DiagnoalElements.java
@@ -1,11 +1,14 @@
 class DiagnoalElements{
+
     public static void main(String[] args){
         int arr[][]={{1,2,3,4,5},
                      {4,5,6,3,2},
                      {7,8,9,5,3},
                      {5,3,2,8,9},
                      {4,9,3,2,1}};
+
         for(int i=0;i<arr.length;i++){
+
             for(int j=0;j<arr.length;j++){
                 if(i==j){
                 System.out.print(arr[i][j]+" ");
@@ -18,4 +21,5 @@ class DiagnoalElements{
             System.out.println();
         }
     }
+    
 }
