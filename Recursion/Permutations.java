a1264c5ed959ab1067a5dfe1044a881587a4efbe
reddirani8496 <reddiranibattuluri@gmail.com>
2026-01-04 15:41:00 +0530

    permutations of string uploaded

--- Recursion/Permutations.java
+++ Recursion/Permutations.java
@@ -1,5 +1,15 @@
 public class Permutations {
     public static void main(String[] args) {
         String str = "abc";
+        permutationOfString(str, "");
+    }
+      public static void permutationOfString(String str, String ans) {
+        if(str.isEmpty()) {
+            System.out.println(ans+" ");
+            return;
+        }
+        for(int i=0;i<str.length();i++) {
+            permutationOfString( str.substring(0,i) + str.substring(i+1), ans + str.charAt(i));
+        }
     }
 }
