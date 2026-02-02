4be24d759b547038b023e582156b812e73a77495
reddirani8496 <reddiranibattuluri@gmail.com>
2026-02-02 08:48:57 +0530

    detect loop uploaded

---
+++ Linkedlist/DetectLoop.java
@@ -0,0 +1,32 @@
+package Linkedlist;
+
+public class DetectLoop {
+    /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+      if(head!=null) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null) {
+            
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow) {
+                return true;
+            }
+        }
+      }
+      return false;
+    }
+}
+}
